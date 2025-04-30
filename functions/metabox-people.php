c6f477d9948e2a9cb5472f13ab2e7782e5807907
Sean Cloudsdale <sean@cloudsdale.co.uk>
2025-04-30 10:00:18 +0100

    bf pvid splide

--- functions/metabox-people.php
+++ functions/metabox-people.php
@@ -2,15 +2,6 @@
 // Add metaboxes for the "People" custom post type
 function add_people_metaboxes() {
     add_meta_box(
-        'people_qualifications_metabox', // Metabox ID
-        'Qualifications',                // Title
-        'render_people_qualifications_metabox', // Callback function
-        'person',                        // Post type
-        'normal',                        // Context (normal, side, etc.)
-        'high'                           // Priority
-    );
-
-    add_meta_box(
         'people_bio_metabox', // Metabox ID
         'Bio',                // Title
         'render_people_bio_metabox', // Callback function
@@ -18,22 +9,17 @@ function add_people_metaboxes() {
         'normal',             // Context (normal, side, etc.)
         'high'                // Priority
     );
+
+    add_meta_box(
+        'people_videos_metabox', // Metabox ID
+        'YouTube Videos',        // Title
+        'render_people_videos_metabox', // Callback function
+        'person',                // Post type
+        'normal',                // Context (normal, side, etc.)
+        'high'                   // Priority
+    );
 }
 add_action('add_meta_boxes', 'add_people_metaboxes');
-
-// Render the "Qualifications" metabox
-function render_people_qualifications_metabox($post) {
-    // Retrieve the current value of the "Qualifications" field
-    $qualifications = get_post_meta($post->ID, '_people_qualifications', true);
-
-    // Add a nonce field for security
-    wp_nonce_field('save_people_qualifications', 'people_qualifications_nonce');
-    ?>
-<label for="people_qualifications">Enter the qualifications:</label>
-<input type="text" id="people_qualifications" name="people_qualifications"
-    value="<?php echo esc_attr($qualifications); ?>" style="width: 100%;" />
-<?php
-}
 
 // Render the "Bio" metabox
 function render_people_bio_metabox($post) {
@@ -48,21 +34,55 @@ function render_people_bio_metabox($post) {
 <?php
 }
 
-// Save the "Qualifications" and "Bio" field values
+// Render the "YouTube Videos" metabox
+function render_people_videos_metabox($post) {
+    // Add a nonce field for security
+    wp_nonce_field('save_people_videos', 'people_videos_nonce');
+
+    // Loop through 4 video slots
+    for ($i = 1; $i <= 4; $i++) {
+        $video_title = get_post_meta($post->ID, "_people_video_{$i}_title", true);
+        $video_description = get_post_meta($post->ID, "_people_video_{$i}_description", true);
+        $video_link = get_post_meta($post->ID, "_people_video_{$i}_link", true);
+        ?>
+<div style="margin-bottom: 20px;">
+    <label for="people_video_<?php echo $i; ?>_title">Video <?php echo $i; ?> Title:</label>
+    <input type="text" id="people_video_<?php echo $i; ?>_title" name="people_video_<?php echo $i; ?>_title"
+        value="<?php echo esc_attr($video_title); ?>" style="width: 100%; margin-bottom: 10px;" />
+
+    <label for="people_video_<?php echo $i; ?>_description">Video <?php echo $i; ?> Description:</label>
+    <input type="text" id="people_video_<?php echo $i; ?>_description" name="people_video_<?php echo $i; ?>_description"
+        value="<?php echo esc_attr($video_description); ?>" style="width: 100%; margin-bottom: 10px;" />
+
+    <label for="people_video_<?php echo $i; ?>_link">Video <?php echo $i; ?> YouTube Link:</label>
+    <input type="url" id="people_video_<?php echo $i; ?>_link" name="people_video_<?php echo $i; ?>_link"
+        value="<?php echo esc_url($video_link); ?>" style="width: 100%;" />
+</div>
+<?php
+    }
+}
+
+// Save the "Bio" and "YouTube Videos" field values
 function save_people_metaboxes($post_id) {
-    // Verify the nonce for "Qualifications"
-    if (isset($_POST['people_qualifications_nonce']) && wp_verify_nonce($_POST['people_qualifications_nonce'], 'save_people_qualifications')) {
-        // Save the "Qualifications" field value
-        if (isset($_POST['people_qualifications'])) {
-            update_post_meta($post_id, '_people_qualifications', sanitize_text_field($_POST['people_qualifications']));
+    // Verify the nonce for "Bio"
+    if (isset($_POST['people_bio_nonce']) && wp_verify_nonce($_POST['people_bio_nonce'], 'save_people_bio')) {
+        if (isset($_POST['people_bio'])) {
+            update_post_meta($post_id, '_people_bio', sanitize_textarea_field($_POST['people_bio']));
         }
     }
 
-    // Verify the nonce for "Bio"
-    if (isset($_POST['people_bio_nonce']) && wp_verify_nonce($_POST['people_bio_nonce'], 'save_people_bio')) {
-        // Save the "Bio" field value
-        if (isset($_POST['people_bio'])) {
-            update_post_meta($post_id, '_people_bio', sanitize_textarea_field($_POST['people_bio']));
+    // Verify the nonce for "YouTube Videos"
+    if (isset($_POST['people_videos_nonce']) && wp_verify_nonce($_POST['people_videos_nonce'], 'save_people_videos')) {
+        for ($i = 1; $i <= 4; $i++) {
+            if (isset($_POST["people_video_{$i}_title"])) {
+                update_post_meta($post_id, "_people_video_{$i}_title", sanitize_text_field($_POST["people_video_{$i}_title"]));
+            }
+            if (isset($_POST["people_video_{$i}_description"])) {
+                update_post_meta($post_id, "_people_video_{$i}_description", sanitize_textarea_field($_POST["people_video_{$i}_description"]));
+            }
+            if (isset($_POST["people_video_{$i}_link"])) {
+                update_post_meta($post_id, "_people_video_{$i}_link", esc_url_raw($_POST["people_video_{$i}_link"]));
+            }
         }
     }
 }
